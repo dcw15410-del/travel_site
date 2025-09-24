32424ae7ad7e6e3a6326ac9dc5a1ee9b30a9889e
dcw15410-del <dcw15410@gmail.com>
2025-09-24 12:29:15 +0900

    fix: update code and requirements

--- app.py
+++ app.py
@@ -306,6 +306,13 @@ def handle_send_message(data):
 # -----------------------------
 # 실행
 # -----------------------------
+from flask import Flask
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app)
+
 
